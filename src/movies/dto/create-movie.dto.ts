5d7abc15b6df9376e7f788464a0c8975e7f6dc4e
jbh0630 <jayjo0630@gmail.com>
2022-02-15 14:57:00 -0800

    add features

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,10 @@
-export class CreateMovieDTO {
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
   readonly title: string;
+  @IsNumber()
   readonly year: number;
+  @IsString({ each: true })
   readonly genres: string[];
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -24,7 +25,7 @@ export class MoviesService {
     return true;
   }
 
-  create(movieData) {
+  create(movieData: CreateMovieDto) {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
